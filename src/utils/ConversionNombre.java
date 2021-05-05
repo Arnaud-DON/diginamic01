93e8bda3ed46477eb93e4d3f7521fec94d154452
Arnaud DON <donnadille.arnaud@gmail.com>
2021-05-05 12:42:14 +0200

    JAVA POO J1_J2 TP04

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int itgr = Integer.parseInt(chaine);
+
+        int a = 4;
+        int b = 65;
+
+        System.out.println(Integer.max(a, b));
+    }
+}
