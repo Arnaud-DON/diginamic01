46e48feae02faf2a871b7ca270fbb458003b916d
Arnaud DON <donnadille.arnaud@gmail.com>
2021-05-05 12:42:33 +0200

    JAVA POO J1_J2 TP05

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+    String accountNb;
+    double balance;
+
+    public Compte(String accountNb, double balance) {
+        this.accountNb = accountNb;
+        this.balance = balance;
+    }
+}
